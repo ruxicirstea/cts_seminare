0ba8e16390b2d24f0d62fbbace053b086c5d576c
Ruxandra <you@example.com>
2021-02-27 14:22:16 +0200

    commit classes

--- sem1_cts/src/clase/Animal.java
+++ sem1_cts/src/clase/Animal.java
@@ -1,5 +1,22 @@
 package clase;
 
-public class Animal {
+public abstract class Animal {
+	private String nume;
+	
+
+	public Animal(String nume) {
+		super();
+		this.nume = nume;
+	}
+
+	public String getNume() {
+		return nume;
+	}
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	
+	
 
 }
--- sem1_cts/src/clase/Zookeeper.java
+++ sem1_cts/src/clase/Zookeeper.java
@@ -1,5 +1,16 @@
 package clase;
 
 public class Zookeeper {
+	private String nume;
+
+	public Zookeeper(String nume) {
+		super();
+		this.nume = nume;
+	}
+	
+	public void feed(Animal a)
+	{
+		System.out.println("Ingrijitorul "+this.nume +" hraneste animalul "+a.getNume());
+	}
 
 }
