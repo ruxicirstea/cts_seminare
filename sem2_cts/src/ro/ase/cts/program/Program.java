aa57b9215a6b5f53427cf62692d0fac03a90e0e7
Ruxandra <you@example.com>
2021-03-06 14:16:36 +0200

    Commit Readers

--- sem2_cts/src/ro/ase/cts/program/Program.java
+++ sem2_cts/src/ro/ase/cts/program/Program.java
@@ -1,17 +1,17 @@
 package ro.ase.cts.program;
 
+
 import java.io.FileNotFoundException;
 import java.util.List;
-
 import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.readers.AngajatReader;
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
